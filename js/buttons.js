09e351e8220a5bcc855c1573805194201c380f75
pomasters <97537718+pomasters@users.noreply.github.com>
2026-04-22 13:12:31 -0400

    Minor additions

--- js/buttons.js
+++ js/buttons.js
@@ -1,26 +1,26 @@
 export const SCOUT_BUTTONS = {
 	"PITY": { "quantity": 1, "costs": {}, "points": 0, "label": "Choose","type": "free2" },
 
-	"DAILYx1": { "quantity": 1, "costs": { "gems": 100 }, "points": 1, "label": "1","type": "orange2" },
+	"DAILYx1": { "quantity": 1, "costs": { "gems": 100 }, "points": 1, "label": "×1","type": "orange2" },
 
-	"GEMx1": { "quantity": 1, "costs": { "gems": 300 }, "points": 3, "label": "1","type": "blue" },
-	"GEMx11": { "quantity": 11, "costs": { "gems": 3000 }, "points": 33, "label": "11","type": "blue" },
+	"GEMx1": { "quantity": 1, "costs": { "gems": 300 }, "points": 3, "label": "×1","type": "blue" },
+	"GEMx11": { "quantity": 11, "costs": { "gems": 3000 }, "points": 33, "label": "×11","type": "blue" },
 
-	"PAIDx1": { "quantity": 1, "costs": { "gems": 100 }, "points": 1, "label": "1","type": "orange" },
-	"PAIDx3": { "quantity": 3, "costs": { "gems": 300 }, "points": 3, "label": "3","type": "orange" },
-	"PAIDx5": { "quantity": 5, "costs": { "gems": 500 }, "points": 5, "label": "5","type": "orange2" },
+	"PAIDx1": { "quantity": 1, "costs": { "gems": 100 }, "points": 1, "label": "×1","type": "orange" },
+	"PAIDx3": { "quantity": 3, "costs": { "gems": 300 }, "points": 3, "label": "×3","type": "orange" },
+	"PAIDx5": { "quantity": 5, "costs": { "gems": 500 }, "points": 5, "label": "×5","type": "orange2" },
 	"PAIDx25": { "quantity": 25, "costs": { "gems": 5000 }, "points": 0, "label": "×25","type": "orange2" },
 
-	"FREEx1": { "quantity": 1, "costs": {}, "points": 0, "label": "1","type": "free" },
-	"FREEx11": { "quantity": 11, "costs": {}, "points": 0, "label": "11","type": "free" },
-	"FREEx31": { "quantity": 31, "costs": {}, "points": 0, "label": "31","type": "free" },
+	"FREEx1": { "quantity": 1, "costs": {}, "points": 0, "label": "×1","type": "free" },
+	"FREEx11": { "quantity": 11, "costs": {}, "points": 0, "label": "×11","type": "free" },
+	"FREEx31": { "quantity": 31, "costs": {}, "points": 0, "label": "×31","type": "free" },
 	"FREEx1_LARGE": { "quantity": 1, "costs": {}, "points": 0, "label": "Daily Scout","type": "free2" },
-	"FREEx11_LARGE": { "quantity": 11, "costs": {}, "points": 0, "label": "11","type": "free2" },
-	"FREEx31_LARGE": { "quantity": 31, "costs": {}, "points": 0, "label": "31","type": "free2" },
+	"FREEx11_LARGE": { "quantity": 11, "costs": {}, "points": 0, "label": "×11","type": "free2" },
+	"FREEx31_LARGE": { "quantity": 31, "costs": {}, "points": 0, "label": "×31","type": "free2" },
 
-	"MASTERSDAY": { "quantity": 11, "costs": { "gems": 25 }, "points": 0, "label": "x11","type": "blue2" },
-	"MASTERSDAY_A": { "quantity": 11, "costs": { "gems": 250 }, "points": 0, "label": "x11","type": "orange2" },
-	"MASTERSDAY_B": { "quantity": 11, "costs": { "gems": 2500 }, "points": 0, "label": "x11","type": "orange2" },
+	"MASTERSDAY": { "quantity": 11, "costs": { "gems": 25 }, "points": 0, "label": "×11","type": "blue2" },
+	"MASTERSDAY_A": { "quantity": 11, "costs": { "gems": 250 }, "points": 0, "label": "×11","type": "orange2" },
+	"MASTERSDAY_B": { "quantity": 11, "costs": { "gems": 2500 }, "points": 0, "label": "×11","type": "orange2" },
 
 	"POKEFAIRx10": { "quantity": 10, "costs": { "tickets": 1 }, "points": 0, "label": "10-Pair Scout","type": "red2" },
 	"SPECIALselectx1": { "quantity": 11, "costs": { "gems": 3000 }, "points": 1, "label": "Special Scout ×11","type": "orange2" },
@@ -29,9 +29,9 @@ export const SCOUT_BUTTONS = {
 	"MASTERFAIRx1": { "quantity": 1, "costs": { "tickets": 1 }, "points": 0, "label": "Master Fair Guaranteed","type": "red2" },
 	"MASTERFAIRx30": { "quantity": 30, "costs": { "gems": 10000 }, "points": 1, "label": "×30","type": "orange2" },
 
-	"TICKETx1": { "quantity": 1, "costs": { "tickets": 1 }, "points": 0, "label": "1","type": "red" },
-	"TICKETx5": { "quantity": 5, "costs": { "tickets": 5 }, "points": 0, "label": "5","type": "red" },
-	"TICKETx10": { "quantity": 10, "costs": { "tickets": 10 }, "points": 0, "label": "10","type": "red2" },
-	"TICKETx1_LARGE": { "quantity": 1, "costs": { "tickets": 1 }, "points": 0, "label": "1","type": "red2" },
-	"TICKETx5_LARGE": { "quantity": 5, "costs": { "tickets": 5 }, "points": 0, "label": "5","type": "red2" }
+	"TICKETx1": { "quantity": 1, "costs": { "tickets": 1 }, "points": 0, "label": "×1","type": "red" },
+	"TICKETx5": { "quantity": 5, "costs": { "tickets": 5 }, "points": 0, "label": "×5","type": "red" },
+	"TICKETx10": { "quantity": 10, "costs": { "tickets": 10 }, "points": 0, "label": "×10","type": "red2" },
+	"TICKETx1_LARGE": { "quantity": 1, "costs": { "tickets": 1 }, "points": 0, "label": "×1","type": "red2" },
+	"TICKETx5_LARGE": { "quantity": 5, "costs": { "tickets": 5 }, "points": 0, "label": "×5","type": "red2" }
 }
